603c945248fd0f4d5ef5ac868be7e25a6d39d9b7
rahuljsaliaan <rahuljsaliaan@gmail.com>
2025-04-21 02:02:20 +0530

    fix(template_utils): :bug: Resolve, Internal Server Error: URL.replace() takes 1 positional argument but 4 were given

--- src/first_glance/utils/template_utils.py
+++ src/first_glance/utils/template_utils.py
@@ -3,8 +3,5 @@ from fastapi import Request
 
 
 def https_url_for(request: Request, name: str, **path_params: Any) -> str:
-
     http_url = request.url_for(name, **path_params)
-
-    # Replace 'http' with 'https'
-    return http_url.replace("http", "https", 1)
+    return str(http_url).replace("http://", "https://", 1)
